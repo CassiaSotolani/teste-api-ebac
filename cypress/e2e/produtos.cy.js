603286f38dfa797d3ef7130e156756bf48f96d65
Cassia Sotolani Matos <cassia.matos@vsoft.com.br>
2025-09-27 17:16:56 -0300

    Correção

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -60,7 +60,7 @@ describe('Testes da Funcionalidade Produtos', () => {
                 headers: {authorization: token}, 
                 body: 
                 {
-                    "nome": "Produto Editado 45642083",
+                    "nome": "Logitech MX Vertical",
                     "preco": 100,
                     "descricao": "Produto editado",
                     "quantidade": 100
